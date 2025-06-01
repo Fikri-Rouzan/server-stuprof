191eaf2b9cb6de93ed0fb894eb75939c39839ee5
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-06-01 14:58:45 +0700

    twenty-second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
-
   getHello(): string {
-    const dbUrl = this.configService.get<string>('MONGODB_URL');
-    const jwtSecret = this.configService.get<string>('JWT_SECRET');
-
-    console.log('MongoDB URL from .env:', dbUrl);
-    console.log('JWT Secret from .env:', jwtSecret);
-
-    return 'Hello World with Config!';
+    return 'Hello World!';
   }
 }
